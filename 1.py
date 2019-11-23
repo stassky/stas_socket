5b4b0a09f179e24856c578080c7603965ff4bedb
Stas <stasyam@gmail.com>
2019-11-23 16:16:40 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -6,12 +6,32 @@ print(datetime.datetime.now())
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
 PORT = 65432       # Port to listen
 # Create a TCP/IP socket
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # ??
-    # s.bind((HOST, PORT))                        ## Associate with spesific Host and Port
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind((HOST, PORT))                        ## Associate with spesific Host and Port
     s.listen()
+
     conn, addr = s.accept()                     ## Server accepts connection and Listen
     with conn:
         print('Connected by IP: ', addr)
 
         conn.close()
 
+# CODE = {'A': '.-',     'B': '-...',   'C': '-.-.',
+#         'D': '-..',    'E': '.',      'F': '..-.',
+#         'G': '--.',    'H': '....',   'I': '..',
+#         'J': '.---',   'K': '-.-',    'L': '.-..',
+#         'M': '--',     'N': '-.',     'O': '---',
+#         'P': '.--.',   'Q': '--.-',   'R': '.-.',
+#         'S': '...',    'T': '-',      'U': '..-',
+#         'V': '...-',   'W': '.--',    'X': '-..-',
+#         'Y': '-.--',   'Z': '--..',
+#
+#         '0': '-----',  '1': '.----',  '2': '..---',
+#         '3': '...--',  '4': '....-',  '5': '.....',
+#         '6': '-....',  '7': '--...',  '8': '---..',
+#         '9': '----.'
+#         }
+# morse = ""
+# for x in int(addr):
+#     morse += CODE[x.lower()]+ " "
+#     print("Morse: " + morse)
