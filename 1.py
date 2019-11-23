c48f166422909a3890c7bf83a8f258bfa21dfa44
Stas <stasyam@gmail.com>
2019-11-23 13:15:16 +0200

    Update 1.py

--- 1.py
+++ 1.py
@@ -4,10 +4,10 @@ import socket
 print(datetime.datetime.now())
 
 HOST = '127.0.0.1'  # Standard loopback interface address (localhost)
-PORT = 30080        # Port to listen
-
+PORT = 30080       # Port to listen
+# Create a TCP/IP socket
 with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:    # ??
-    s.bind((HOST, PORT))                        ## Associate with spesific Host and Port
+    # s.bind((HOST, PORT))                        ## Associate with spesific Host and Port
     s.listen()
     conn, addr = s.accept()                     ## Server accepts connection and Listen
     with conn:
